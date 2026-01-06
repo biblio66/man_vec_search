5923cf0c9bd8daa23035655dfeb0bfbe882ce9ec
biblio66 <salahuddin66@bell01.com>
2026-01-06 13:08:15 +0100

    Update man_vec_search.py

--- man_vec_search.py
+++ man_vec_search.py
@@ -18,8 +18,8 @@ def main():
     if not document_lines:
         print("Error: No input data received from stdin.")
         sys.exit(1)
-    pattern = re.compile(r'[-()]+')
-    formatted_lines = [pattern.sub(' ', line.lower()) for line in document_lines]
+
+    formatted_lines = [line.lower().replace("-", " ").replace("(", " ").replace(")", " ").replace("\'s ", " ").replace("'", " ").replace("\"", "").replace(".", " .") for line in document_lines]
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", type=str, required=True, help="Text to search for")
